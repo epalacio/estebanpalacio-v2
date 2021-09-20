e1f92b28b9c8a0267ee73e711e22f562dfab8205
Esteban Palacio <estipalacio@gmail.com>
2021-09-20 12:04:15 -0400

    added main components and simple structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainContent from './components/mainContent/mainContent.component';
+import Navbar from './components/navbar/navbar.component';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
-        <h3 className="text-buttercup text-7xl">Esteban Palacio</h3>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+	<Router>
+	<div>
+		<Navbar/>
+		<MainContent/>
+	</div>
+	</Router>
+);
 
 export default App;
